91760e5f84d9a6429fa761c1b60c53009cf55366
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-25 11:49:42 +0200

    exercici 1 tables

--- Entrega2-1.sql
+++ Entrega2-1.sql
@@ -1,9 +1,27 @@
 -- Exercici 1 - Òptica
 CREATE TABLE proveïdor (
-    id INTEGER NOT NULL AUTO_INCREMENT, 
     nom TEXT,
     adreça TEXT,
     telèfon INTEGER,
     fax INTEGER,
     nif INTEGER
-)
+);
+
+CREATE TABLE ulleres (
+    marca TEXT,
+    graduacio INTEGER,
+    montura TEXT,
+    color_montura TEXT,
+    color_vidre TEXT,
+    preu INTEGER
+);
+
+CREATE TABLE clients (
+    nom TEXT,
+    adreça TEXT,
+    telèfon INTEGER,
+    email TEXT,
+    data_registre INTEGER,
+    recomenat TEXT,
+    venta TEXT
+);
