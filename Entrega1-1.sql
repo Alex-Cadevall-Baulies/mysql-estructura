a09479501613433c627f2539582a25061df6a3a9
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-25 11:09:34 +0200

    inici 2-1

---
+++ Entrega1-1.sql
@@ -0,0 +1,9 @@
+-- Exercici 1 - Òptica
+CREATE TABLE proveïdor (
+    id INTEGER NOT NULL AUTO_INCREMENT, 
+    nom TEXT,
+    adreça TEXT,
+    telèfon INTEGER,
+    fax INTEGER,
+    nif INTEGER
+)
