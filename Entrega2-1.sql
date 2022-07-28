1f4814f54c3307870078a64b40abee953d9dfbf6
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-28 11:00:58 +0200

    botigues table finished

--- Entrega2-1.sql
+++ Entrega2-1.sql
@@ -1,79 +1,98 @@
 -- Exercici 1 - Òptica
-CREATE TABLE proveidor_òptica (
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    nom TEXT,
-    adreça TEXT,
-    telèfon INTEGER,
-    fax INTEGER,
-    nif INTEGER,
-    PRIMARY KEY (id)
-);
 
-CREATE TABLE ulleres_òptica (
-    id_proveidor INT UNSIGNED NOT NULL,
-    FOREIGN KEY (id_proveidor) REFERENCES proveidor_òptica(id),
-    marca TEXT,
-    graduacio INTEGER,
-    montura TEXT,
-    color_montura TEXT,
-    color_vidre TEXT,
-    preu INTEGER
-);
+CREATE TABLE
+    proveidor_òptica (
+        id INTEGER NOT NULL AUTO_INCREMENT,
+        nom TEXT,
+        adreça TEXT,
+        telèfon INTEGER,
+        fax INTEGER,
+        nif INTEGER,
+        PRIMARY KEY (id)
+    );
 
-CREATE TABLE client_òptica (
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    nom TEXT,
-    adreça TEXT,
-    telèfon INTEGER,
-    email TEXT,
-    data_registre DATE,
-    recomenat TEXT,
-    venta TEXT,
-    PRIMARY KEY (id)
-);
+CREATE TABLE
+    ulleres_òptica (
+        id_proveidor INT UNSIGNED NOT NULL,
+        FOREIGN KEY (id_proveidor) REFERENCES proveidor_òptica(id),
+        marca TEXT,
+        graduacio INTEGER,
+        montura TEXT,
+        color_montura TEXT,
+        color_vidre TEXT,
+        preu INTEGER
+    );
+
+CREATE TABLE
+    client_òptica (
+        id INTEGER NOT NULL AUTO_INCREMENT,
+        nom TEXT,
+        adreça TEXT,
+        telèfon INTEGER,
+        email TEXT,
+        data_registre DATE,
+        recomenat TEXT,
+        venta TEXT,
+        PRIMARY KEY (id)
+    );
 
 -- Exercici 2 - Pizzeria
 
-CREATE TABLE client_pizzeria (
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    nom TEXT,
-    cognom1 TEXT,
-    cognom2 TEXT,
-    adreça TEXT,
-    codi_postal INTEGER,
-    id_localitat INTEGER NOT NULL UNIQUE,
-    localitat TEXT NOT NULL,
-    id_provincia INTEGER NOT NULL UNIQUE,
-    provincia TEXT NOT NULL,
-    telèfon INTEGER,
-    PRIMARY KEY (id)
-);
+CREATE TABLE
+    client_pizzeria (
+        id INTEGER NOT NULL AUTO_INCREMENT,
+        nom TEXT,
+        cognom1 TEXT,
+        cognom2 TEXT,
+        adreça TEXT,
+        codi_postal INTEGER,
+        id_localitat INTEGER NOT NULL UNIQUE,
+        localitat TEXT NOT NULL,
+        id_provincia INTEGER NOT NULL UNIQUE,
+        provincia TEXT NOT NULL,
+        telèfon INTEGER,
+        PRIMARY KEY (id)
+    );
 
-CREATE TABLE comandes_pizzeria (
-    id_comanda INTEGER NOT NULL,
-    data_comanda DATE,
-    entrega INTEGER,
-    productes TEXT,
-    quantitat_productes INTEGER,
-    preu INTEGER,
-    id_cliente INTEGER NOT NULL,
-    FOREIGN KEY (id_cliente) REFERENCES client_pizzeria(id)
-);
+CREATE TABLE
+    comandes_pizzeria (
+        id_comanda INTEGER NOT NULL,
+        data_comanda DATE,
+        entrega INTEGER,
+        productes TEXT,
+        quantitat_productes INTEGER,
+        preu INTEGER,
+        id_cliente INTEGER NOT NULL,
+        FOREIGN KEY (id_cliente) REFERENCES client_pizzeria(id)
+    );
 
-CREATE TABLE categoria_pizzeria (
-id_categoria INTEGER NOT NULL,
-nom_categoria VARCHAR(400) NOT NULL,
-PRIMARY KEY (nom_categoria)
-);
+CREATE TABLE
+    categoria_pizzeria (
+        id_categoria INTEGER NOT NULL,
+        nom_categoria VARCHAR(400) NOT NULL,
+        PRIMARY KEY (nom_categoria)
+    );
 
+CREATE TABLE
+    productes_pizzeria (
+        id_producte INTEGER NOT NULL AUTO_INCREMENT,
+        nom TEXT,
+        categoria_pizza VARCHAR(400),
+        FOREIGN KEY (categoria_pizza) REFERENCES categoria_pizzeria(nom_categoria),
+        descripció TEXT,
+        imatge VARCHAR(2038),
+        preu INTEGER,
+        PRIMARY KEY (id_producte)
+    );
 
-CREATE TABLE productes_pizzeria (
-    id_producte INTEGER NOT NULL AUTO_INCREMENT,
-    nom TEXT,
-    categoria_pizza VARCHAR(400),
-    FOREIGN KEY (categoria_pizza) REFERENCES categoria_pizzeria(nom_categoria),
-    descripció TEXT,
-    imatge VARCHAR(2038),
-    preu INTEGER,
-    PRIMARY KEY (id_producte)
-);
+CREATE TABLE
+    botigues_pizzeria (
+        id_botiga INTEGER NOT NULL,
+        adreça VARCHAR(400),
+        codi_postal INTEGER,
+        id_localitat INTEGER NOT NULL UNIQUE,
+        localitat TEXT NOT NULL,
+        id_provincia INTEGER NOT NULL UNIQUE,
+        provincia TEXT NOT NULL,
+        PRIMARY KEY(id_botiga)
+    );
