31c1b0937617ffb3da8693fb8654ed5408adeff8
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-08-02 11:36:45 +0200

    trying to connet to the server

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
-const qbRoutes = require("./routes/qb");
+const entregaRoutes = require("./routes/entrega");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use(qbRoutes);
+app.use(entregaRoutes);
 
 app.listen(4000);
--- utils/database.js
+++ utils/database.js
@@ -4,7 +4,7 @@ const mysqlConnection = mysql.createConnection({
   host: "localhost",
   user: "root",
   database: "Entrega2-1",
-  password: "ENTER YOUR MYSQL DB PASSWORD HERE",
+  password: "",
   multipleStatements: true,
 });
 
